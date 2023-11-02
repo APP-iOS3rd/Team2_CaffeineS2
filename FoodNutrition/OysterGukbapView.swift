0549db5fa0c9f84df45de67dd71b8b8e89d838e4
com.naver <com.naver>
2023-11-03 00:30:35 +0900

    굴국밥

--- FoodNutrition/OysterGukbapView.swift
+++ FoodNutrition/OysterGukbapView.swift
@@ -13,6 +13,7 @@ struct OysterGukbapView: View {
 	
 	var body: some View {
 		Text(nutrition.descKor)
+        Link("굴 축제 정보", destination: URL(string:"https://www.brcn.go.kr/tour/sub03_01_01.do")! )
 	}
 }
 

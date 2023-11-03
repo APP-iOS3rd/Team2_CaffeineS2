5c4623ec23aef9b88adee6e9b7663671fc9b12b5
MJKim <qorhaqt@gmail.com>
2023-11-03 10:26:54 +0900

    만둣국으로 헷갈려서 그만 데헷;

--- FoodNutrition/GrilledBeefView.swift
+++ FoodNutrition/GrilledBeefView.swift
@@ -11,8 +11,39 @@ struct GrilledBeefView: View {
 	
 	@Binding var nutrition: Nutrition
 	
+	@State private var searchList: [Item] = []
+	
 	var body: some View {
-		Text(nutrition.descKor)
+		List {
+			ForEach(0..<searchList.count, id: \.self) { i in
+				HStack {
+					VStack(alignment: .leading) {
+						Text(searchList[i].title.removeTag)
+							.font(.headline)
+						Text(searchList[i].roadAddress)
+							.font(.caption)
+							.foregroundStyle(.gray)
+					}
+				}
+			}
+			.navigationTitle(Text("소양념갈비구이 맛집 리스트"))
+		}
+		.onAppear {
+			searchList = DataManager.shared.searchResult?.items ?? []
+		}
+	}
+}
+
+extension String {
+	var removeTag: String {
+		var value: String = self
+		while value.contains("<b>") {
+			value = value.replacingOccurrences(of: "<b>", with: "")
+		}
+		while value.contains("</b>") {
+			value = value.replacingOccurrences(of: "</b>", with: "")
+		}
+		return value
 	}
 }
 

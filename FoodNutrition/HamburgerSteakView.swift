e485181287520e8289d4e20fd416a5cf6616a4d5
Jihunkim95 <darak551@naver.com>
2023-11-03 10:38:28 +0900

    깃 테스트 주석처리
    깃 테스트 주석처리

--- FoodNutrition/HamburgerSteakView.swift
+++ FoodNutrition/HamburgerSteakView.swift
@@ -13,7 +13,7 @@ struct HamburgerSteakView: View {
 	
 	var body: some View {
         Section(header: Text("맛집 정보")) {
-//            Text("")
+//            Text("깃 크라켄 테스트를 위한 주석")
             List{
                 Link("구슬함박 파미에파크점", destination: URL(string: "https://map.naver.com/p/search/%ED%96%84%EB%B2%84%EA%B1%B0%20%EC%8A%A4%ED%85%8C%EC%9D%B4%ED%81%AC%20%EB%A7%9B%EC%A7%91/place/31375404?placePath=?entry=pll&from=nx&fromNxList=true&c=15.00,0,0,0,dh")!)
                 Link("오른손푸드카페", destination: URL(string: "https://map.naver.com/p/search/%ED%96%84%EB%B2%84%EA%B1%B0%20%EC%8A%A4%ED%85%8C%EC%9D%B4%ED%81%AC%20%EB%A7%9B%EC%A7%91/place/31601162?placePath=?entry=pll&from=nx&fromNxList=true&c=15.00,0,0,0,dh")!)
